e4d6ff5c82791700c0243c410c193b12b777d0d1
Shaun Spinelli <shaun@sitesee.io>
2020-04-02 20:26:54 +1000

    Added utility functions

---
+++ herbarium_2020/datatset.py
@@ -0,0 +1,36 @@
+# --- 100 characters ------------------------------------------------------------------------------
+# Created by: Shaun 2019/01/01
+
+import numpy as np
+from torch.utils.data import Dataset, DataLoader, TensorDataset
+import torch
+
+from PIL import Image
+
+
+class HerbDataSet(Dataset):
+    def __init__(self, data_frame, location, size, x_name="file_name", y_name="label"):
+        """
+
+        Args:
+            data_frame (Dataframe): Dataframe containing label and data
+            location (str): data location directory
+            size (int): size of image
+            x_name (str): column name of file
+            y_name (str): column name of label
+        """
+        self.df = data_frame
+        self.location = location
+        self.size = (size, size)
+        self.x_name = x_name
+        self.y_name = y_name
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, idx):
+        item = self.df.iloc[idx]
+        image = np.array(Image.open("{}/{}".format(self.location, item[self.x_name])).resize(self.size))
+        image_tensor = torch.tensor(image/255, dtype=torch.float)
+        return image_tensor, item[self.y_name]
+
